9496c8bdde1f9e6f8586040d6d89bfa2cc9a6083
andreferrita-collab <andreferrita@gmail.com>
2026-05-23 13:30:00 -0300

    Implement language-based greeting in PlayState
    Added multilingual support for greeting messages based on system language.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,15 +2,51 @@ package;
 
 import flixel.FlxState;
 import flixel.text.FlxText;
+import openfl.system.Capabilities;
 
 class PlayState extends FlxState
 {
 	override public function create()
-   {
-	super.create();
+	{
+		super.create();
 
-	var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
-	text.screenCenter();
-	add(text);
-    }
+		var language:String = Capabilities.language;
+
+		var message:String = "Hello World";
+
+		switch (language)
+		{
+			case "pt", "pt-BR":
+				message = "Olá Mundo";
+
+			case "es":
+				message = "Hola Mundo";
+
+			case "fr":
+				message = "Bonjour le monde";
+
+			case "de":
+				message = "Hallo Welt";
+
+			case "it":
+				message = "Ciao Mondo";
+
+			case "ja":
+				message = "こんにちは世界";
+
+			case "ru":
+				message = "Привет мир";
+
+			case "zh-CN":
+				message = "你好，世界";
+
+			default:
+				message = "Hello World";
+		}
+
+		// Cria o texto na tela
+		var text = new FlxText(0, 0, 0, message, 64);
+		text.screenCenter();
+		add(text);
+	}
 }
